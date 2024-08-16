e5b5e6a4cf8a1ce0a978fc637908e446b7730e58
Jean Carlos <contato.jeancl25@gmail.com>
2024-08-16 15:21:41 -0300

    feat: ajuste na rota home

--- front-end/src/router/Router.jsx
+++ front-end/src/router/Router.jsx
@@ -8,8 +8,8 @@ const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} >
-        <Route path="/calendar" element={<Calendar />} />
-        <Route path="/home" index element={<TelaInicial />} />
+        <Route path="calendar" element={<Calendar />} />
+        <Route path="home" index element={<TelaInicial />} />
       </Route>
       <Route path="/login" element={<Login />} />
     </Routes>
