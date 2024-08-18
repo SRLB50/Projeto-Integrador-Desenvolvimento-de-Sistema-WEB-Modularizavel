5584c4e953dbbd3fd00b7f3803a32e6106d411ca
Jean Carlos <contato.jeancl25@gmail.com>
2024-08-18 14:43:35 -0300

    feat: inserção de chamada de api para ciclo

--- front-end/src/components/TelaInicial/TelaInicial.jsx
+++ front-end/src/components/TelaInicial/TelaInicial.jsx
@@ -4,13 +4,15 @@ import CardBlog from "../CardBlog/CardBlog"
 import {jwtDecode} from "jwt-decode"
 import "./TelaInicial.scss"
 import { useNavigate } from "react-router-dom"
+import Ciclo from "../../services/ciclo"
+import Cards from "./../../mocks/cards.json"
 
 const TelaInicial = () => {
     const navigate = useNavigate()
     const [day, month, year] = new Date().toLocaleDateString().split("/")
     const dayWeek = new Date().getDay()
     const formattedMonth = getFormattedMonth(Number(month) - 1)
-    const cards = cardsBlog()
+    const cards = Cards
     
     const [name, setName] = useState()
     const [id, setId] = useState()
@@ -20,15 +22,23 @@ const TelaInicial = () => {
         const token = sessionStorage.getItem("token")
         if (token) {
             const decodeToken = jwtDecode(token)
-            const {nome, id, ciclo} = decodeToken
+            const {nome, id} = decodeToken
             
+            getCiclo(id)
             setName(nome)
             setId(id)
-            setCiclo(ciclo)
         }else{
             navigate("/login")
         }
     }, [])
+
+    const getCiclo = async (id) => {
+        const requestCiclo = new Ciclo(id)
+            
+        const ciclo = await requestCiclo.get()
+
+        setCiclo(ciclo.dias)
+    }
 
     return (
         <section id="tela_inicial">
@@ -70,31 +80,6 @@ const TelaInicial = () => {
         </section>
     )
 }
-
-const cardsBlog = () => {
-    return [
-        {
-            title: "Vida saudável",
-            img: "https://images.pexels.com/photos/864939/pexels-photo-864939.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            subtitle: "03/04/2024 - Michel K. | 4min",
-            text: "Quer uma vida saudável e sem problemas? Vem conosco e se prepare para a mudança de vida!"
-        },
-        {
-            title: "Cuide-se",
-            img: "https://images.pexels.com/photos/774866/pexels-photo-774866.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            subtitle: "02/01/2024 - Sandra R. | 7 min",
-            text: "Se cuidar deve ser um dos primordios femininos. Que tal algumas dicas?"
-        },
-        {
-            title: "Trabalho remoto",
-            img: "https://images.pexels.com/photos/1586973/pexels-photo-1586973.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-            subtitle: "10/07/2023 - Gabriel A. | 5 min",
-            text: `O mercado de trabalho remoto para as mulheres cresceu exponencialmente! Veja as vagas que mais permitem o trabalho home office.`
-        }
-    ]
-
-}
-
 
 const getFormattedMonth = (month) => {
 
