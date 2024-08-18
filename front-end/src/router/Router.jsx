5ef779702b7fd043e2d82b1bd273040dd2e50577
Jean Carlos <contato.jeancl25@gmail.com>
2024-08-18 14:43:59 -0300

    feat: inserção de novas rotas

--- front-end/src/router/Router.jsx
+++ front-end/src/router/Router.jsx
@@ -1,15 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from '../views/home/index';
 import Login from '../views/login/index';
-import Calendar from '../views/calendar/index'
 import TelaInicial from '../components/TelaInicial/TelaInicial';
+import Perfil from '../components/Perfil/Perfil';
+import Dicas from '../components/Dicas/Dicas';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} >
-        <Route path="calendar" element={<Calendar />} />
+        {/* <Route path="calendar" element={<Calendar />} /> */}
         <Route path="home" index element={<TelaInicial />} />
+        <Route path='perfil' element={<Perfil />} />
+        <Route path='dicas' element={<Dicas />} />
       </Route>
       <Route path="/login" element={<Login />} />
     </Routes>
