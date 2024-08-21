3e637ef215dda3f69e630eac15aa38e73386dc6b
Jean Carlos <contato.jeancl25@gmail.com>
2024-08-21 19:17:57 -0300

    feat: equalização de versões

--- front-end/src/router/Router.jsx
+++ front-end/src/router/Router.jsx
@@ -2,17 +2,15 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../views/home/index';
 import Login from '../views/login/index';
 import TelaInicial from '../components/TelaInicial/TelaInicial';
-import Perfil from '../components/Perfil/Perfil';
-import Dicas from '../components/Dicas/Dicas';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} >
-        {/* <Route path="calendar" element={<Calendar />} /> */}
+        <Route path="calendar" element={<TelaInicial />} />
         <Route path="home" index element={<TelaInicial />} />
-        <Route path='perfil' element={<Perfil />} />
-        <Route path='dicas' element={<Dicas />} />
+        <Route path='perfil' element={<TelaInicial />} />
+        <Route path='dicas' element={<TelaInicial />} />
       </Route>
       <Route path="/login" element={<Login />} />
     </Routes>
