508abd8b8b1c2a0d22935a4f9720ebb39adc06ce
michelkruger <micheldequadros@gmail.com>
2024-08-26 08:56:15 -0300

    adição do dia inicial, ao carregar componente

--- front-end/src/router/Router.jsx
+++ front-end/src/router/Router.jsx
@@ -8,13 +8,12 @@ const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} >
-        <Route path="calendar" element={<TelaInicial />} />
+        <Route path="calendar" element={<Calendario />} />
         <Route path="home" index element={<TelaInicial />} />
         <Route path='perfil' element={<TelaInicial />} />
         <Route path='dicas' element={<TelaInicial />} />
       </Route>
       <Route path="/login" element={<Login />} />
-      <Route path="/calendario" element={<Calendario />} />
     </Routes>
   );
 };
--- front-end/src/views/calendario/index.jsx
+++ front-end/src/views/calendario/index.jsx
@@ -8,14 +8,23 @@ const Calendario = () => {
     const [count, setCount] = useState(0)
     const [textExample, setTextExample] = useState("")
 
+    const formatDate = (date) => {
+      const day = date.getDate().toString().padStart(2, '0');
+      const month = date.toLocaleString('pt-BR', { month: 'long' });
+      const year = date.getFullYear();
+      return { day, month, year };
+  };
+
+  const { day, month, year } = formatDate(new Date());
+
     return (
       <div className="container-calendar-screen">
-        <div>
-          <h2>Como está o seu cíclo hoje, Marcela?</h2>
-        </div>
-        <div>
-          <h3>04 de Junho 2024</h3>
-          <h3>Faltam 9 dias para sua menstruação.</h3>
+        <div className="welcome-user-container">
+          <h2 className="main-title">Como está o seu cíclo hoje, <span className="bold-phrase prominence-word">Marcela</span>?</h2>
+          <div className="counter-day-container">
+            <h3 className="bold-phrase">{day} de {month} <span className="prominence-word">{year}</span></h3>
+            <h3>Faltam <span>9</span> dias para sua menstruação.</h3>
+          </div>
         </div>
         <div className="">
           <CalendarioComponent year={2024} month={7} />
