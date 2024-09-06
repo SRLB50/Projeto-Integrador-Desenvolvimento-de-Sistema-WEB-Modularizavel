f130e497a159e4af272954ed0b30d24fb01cd2b0
JeanC777 <jean.silva@erpworks.com.br>
2024-09-06 11:20:31 -0300

    feat: ajuste na view de dicas

--- front-end/src/views/dicas/index.jsx
+++ front-end/src/views/dicas/index.jsx
@@ -1,48 +1,51 @@
-/* eslint-disable */
-import { useNavigate } from 'react-router-dom'
-
-import CardBlog from "../../components/CardBlog/CardBlog"
-import Cards from "../../mocks/cards.json"
 import "./index.scss"
+import cards from "./../../mocks/cards.json"
+import CardBlog from "../../components/CardBlog/CardBlog"
+import { useEffect, useState } from "react"
+import Blog from "../../components/Blog/Blog"
 
 const Dicas = () => {
-  const navigate = useNavigate()
+
+  const [blog, setBlog] = useState(false)
+  const [idBlog, setIdBlog] = useState("")
+
+  useEffect(() => setBlog(false), [])
 
   const handleClick = (id) => {
-    console.log(id, 'id')
-    if(id === 1) {
-      navigate("/dicas/vida-saudavel")
-    } else if (id === 2) {
-      navigate("/dicas/cuida-se")
-    } else {
-      navigate("/dicas/trabalho-remoto")
-    }
+    setIdBlog(id)
+    setBlog(true)
   }
 
   return (
-    <div className="dicas-Container">
-      <div id="dicas-saude">
-        <div className="titulo-saude">
-          <h3>
-            Dicas de <span className="pink-text">Saúde</span>
-          </h3>
-        </div>
-        <div className="cards">
-          {Cards.map((card, i) => (
-            <CardBlog
-              img={card.img}
-              title={card.title}
-              subtitle={card.subtitle}
-              text={card.text}
-              key={i}
-              id={card.id}
-              handleClick={handleClick}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
+    <div id="dicas-saude">
+      {
+        !blog
+        && (
+          <div className="titulo-saude">
+            <h3>Dicas de <span className="pink-text">Saúde</span></h3>
+          </div>
+        )
+      }
+      <div className="cards">
+        {
+          !blog ?
+            cards.map((card, i) => (
+              <CardBlog
+                img={card.img}
+                title={card.title}
+                subtitle={card.subtitle}
+                text={card.text}
+                key={i}
+                handleClick={() => handleClick(card.id)}
+              />
+            ))
 
-export default Dicas;
+            : <Blog id={idBlog} setBlogFalse={() => setBlog(false)} />
+        }
+      </div>
+
+    </div>
+  )
+}
+
+export default Dicas
